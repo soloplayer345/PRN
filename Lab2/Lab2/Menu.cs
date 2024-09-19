945fd50b64fbcc97e6042ee37897b2d5628094dd
soloplayer345 <hongtlse172336@fpt.edu.vn>
2024-09-20 03:14:05 +0700

    add lab2 code

---
+++ Lab2/Lab2/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Menu
+    {
+        public int getIntChoice(ArrayList list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i+1} : ${list.IndexOf(i)}");
+            }
+            Console.WriteLine($"Please choose 1 to {list.Count}: ");
+            Console.WriteLine("---------------------------");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("12");
+            list.Add("13");
+            list.Add("14");
+            Menu menu = new Menu();
+            int choice = menu.getIntChoice(list);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
