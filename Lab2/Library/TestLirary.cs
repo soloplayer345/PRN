192eb9003a30e891164d0bcc43c4d25d24837857
Larry <90505134+soloplayer345@users.noreply.github.com>
2024-09-22 13:03:19 +0700

    create library

---
+++ Lab2/Library/TestLirary.cs
@@ -0,0 +1,19 @@
+﻿using Lab2;
+using System.Collections;
+
+namespace Library
+{
+    internal class TestLirary
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("12");
+            list.Add("13");
+            list.Add("14");
+            Menu menu = new Menu();
+            int choice = menu.GetIntChoice(list);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
