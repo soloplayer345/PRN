a61b8e188f13f13c2d27a1619f91b8a1b6026731
soloplayer345 <hongtlse172336@fpt.edu.vn>
2024-10-05 02:20:00 +0700

    asm1

---
+++ asm1/asm1/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asm1
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        public User(string name, string address, string email)
+        {
+            Name = name;
+            Address = address;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Password: {Address}, email: {Email}";
+        }
+    }
+}
