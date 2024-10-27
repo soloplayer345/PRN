345016c9a18e06858960a4b6fef352a7154e2cce
Larry <90505134+soloplayer345@users.noreply.github.com>
2024-10-28 01:54:38 +0700

    add asm2

---
+++ ASM2/ASM2/MemberController.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer;
+
+using RepositoryLayer;
+
+namespace PresentationLayer
+{
+    internal class MemberController
+    {
+        private readonly MemberBusinesslayer memberBusinesslayer;
+
+        public Member Create(Member member)
+        {
+            return memberBusinesslayer.Create(member);
+        }
+
+
+        public void Update(int id,Member member)
+        {
+            memberBusinesslayer.Update(id,member);
+        }
+
+        public Member Get(int id)
+        {
+            return memberBusinesslayer.Read(id);
+        }
+
+        public void Delete(int id)
+        {
+            memberBusinesslayer.Delete(id);
+        }
+
+        public List<Member> Get() 
+        {
+            return memberBusinesslayer.Read();
+        }
+
+    }
+}
