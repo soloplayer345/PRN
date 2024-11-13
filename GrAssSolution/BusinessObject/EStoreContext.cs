300ba1d03bb2dbd8fc3c8735817e9864a7bf213f
Larry <90505134+soloplayer345@users.noreply.github.com>
2024-11-14 00:25:57 +0700

    add GpAss

---
+++ GrAssSolution/BusinessObject/EStoreContext.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BusinessObject;
+
+public partial class EStoreContext : DbContext
+{
+    public EStoreContext()
+    {
+    }
+
+    public EStoreContext(DbContextOptions<EStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Member> Members { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       => optionsBuilder.UseSqlServer(GetConnectionString());
+
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+        var strConn = config["ConnectionStrings:DefaultConnectionString"];
+        return strConn;
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Member>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__Member__0CF04B18D790BBD8");
+
+            entity.ToTable("Member");
+
+            entity.Property(e => e.City)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.CompanyName)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Country)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Order__C3905BCFD4F5309A");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.OrderId).ValueGeneratedNever();
+            entity.Property(e => e.Freight).HasColumnType("money");
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.RequiredDate).HasColumnType("datetime");
+            entity.Property(e => e.ShippedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Member).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.MemberId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Order__MemberId__4D94879B");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.ProductId }).HasName("PK__OrderDet__08D097A3A22D8317");
+
+            entity.ToTable("OrderDetail");
+
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__OrderDeta__Order__5070F446");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__OrderDeta__Produ__5165187F");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Product__B40CC6CDD8ED67D3");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+            entity.Property(e => e.Weight)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ GrAssSolution/DataAccess/IMemberRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    internal interface IMemberRepository : IDisposable
+    {
+        IEnumerable<Member> GetMembers();
+        Member GetMember(int id);
+        void AddMember(Member member);
+        void RemoveMember(Member member);
+        void UpdateMember(Member member);
+        void Save();
+    }
+}
