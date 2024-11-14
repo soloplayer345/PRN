593bb877fff94512827b53615a4f21095e7aafe5
soloplayer345 <hongtlse172336@fpt.edu.vn>
2024-11-14 16:24:42 +0700

    up

---
+++ GrAssSolution/DataAccess/MemberRepository.cs
@@ -0,0 +1,47 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class MemberRepository : IMemberRepository, IDisposable
+    {
+        public void AddMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetMember(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Member> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
